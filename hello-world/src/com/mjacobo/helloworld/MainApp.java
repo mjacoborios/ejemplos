6fff919946c85b71c9059f3abd5db4a90cf4a41b
mjacoborios <mjacoborios@gmail.com>
2016-12-02 14:46:17 -0600

    inicial

--- hello-world/src/com/mjacobo/helloworld/MainApp.java
+++ hello-world/src/com/mjacobo/helloworld/MainApp.java
@@ -5,6 +5,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Hello World - Local Modification");
+		System.out.println("Hello World - Other Local Modification");
 	}
 
 }
