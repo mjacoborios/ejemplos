421491143f74a12f38599d0a52cc6acecad71ca6
mjacoborios <mjacoborios@gmail.com>
2016-12-02 14:38:02 -0600

    Commit Inicial de Hello World

---
+++ hello-world/src/com/mjacobo/helloworld/MainApp.java
@@ -0,0 +1,10 @@
+package com.mjacobo.helloworld;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
