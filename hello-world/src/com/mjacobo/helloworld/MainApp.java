bb4a453c670eba4b90a66c03c7e288ee13fdb86f
mjacoborios <mjacoborios@gmail.com>
2016-12-02 14:49:00 -0600

    Remote Modification

--- hello-world/src/com/mjacobo/helloworld/MainApp.java
+++ hello-world/src/com/mjacobo/helloworld/MainApp.java
@@ -7,6 +7,7 @@ public class MainApp {
 		System.out.println("Hello World");
 		System.out.println("Hello World - Local Modification");
 		System.out.println("Hello World - Other Local Modification");
+		System.out.println("Hello World - Remote Modification");
 	}
 
 }
